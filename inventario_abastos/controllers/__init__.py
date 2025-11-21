a370888efb1a6532831426f8a266041c5034e2ff
alecanche04-cyber <alecanche04@gmail.com>
2025-11-21 10:16:39 -0600

    Update __init__.py

--- inventario_abastos/controllers/__init__.py
+++ inventario_abastos/controllers/__init__.py
@@ -5,10 +5,10 @@ from.inventario import check_low_stock, check_expired_items, calculate_order_qty
 from. pedidos import generate_suggested_orders, confirm_orders # Reexportar funciones del módulo pedidos
 
 __all__ = [ # Definir los nombres que se exportan al importar el paquete
-    'check_low_stock', # Reexportar funciones del módulo inventario
-    'check_expired_items', # Reexportar funciones del módulo inventario
-    'calculate_suggested_orders_qty', # Reexportar funciones del módulo inventario
-    'generate_suggested_orders', # Reexportar funciones del módulo pedidos
-    'confirm_orders'    # Reexportar funciones del módulo pedidos
+    "chechk_low_stock", #Reexportar funciones del módulo inventario
+    "check_expired_items", # Reexportar funciones del módulo inventario
+    "calculate_suggested_orders_qty", # Reexportar funciones del módulo inventario
+    "generate_suggested_orders", # Reexportar funciones del módulo pedidos
+    "confirm_orders"    # Reexportar funciones del módulo pedidos
 ]
 
