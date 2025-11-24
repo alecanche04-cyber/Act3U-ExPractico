1175afe62b72ca634d030fc9f4d818d2bc6b2726
melaniebaasaguilar-web <melaniebaasaguilar@gmail.com>
2025-11-24 16:09:25 -0600

    correccion de variable

--- inventario_abastos/routes/__init__.py
+++ inventario_abastos/routes/__init__.py
@@ -1,7 +1,7 @@
 # __init__.py
 # Permite importar los routers desde la carpeta "routes".
 
-from .productos_router import router as productos_router
-from .pedidos_router import router as pedidos_router
+from .producto_routes import router as productos_router
+from .pedido_routes import router as pedidos_router
 
 __all__ = ["productos_router", "pedidos_router"]
